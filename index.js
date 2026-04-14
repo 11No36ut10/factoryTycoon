1ce49f589eeba80ffb3aa58a9c57ae3ba61565df
Nout113610 <145438363+11No36ut10@users.noreply.github.com>
2026-04-14 16:15:44 +0000

    start

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+let pixelRowAmount = 25;
+let pixelSize = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  resizePixels();
+  noStroke();
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 50);
+  background(255);
+  for(let i = 0; i < pixelRowAmount; i++) {
+    for(let t = 0; t < pixelRowAmount; t++) {
+      if(
+        mouseX > pixelSize*i     &&
+        mouseX < pixelSize*(i+1) &&
+        mouseY > pixelSize*t     &&
+        mouseY < pixelSize*(t+1)
+        ){
+        fill(100, 100, 100);
+      }else{
+        fill(48, 48, 48);
+      }
+      rect(pixelSize*i, pixelSize*t, pixelSize, pixelSize);
+    }
+  }
+}
+
+function resizePixels() {
+  if(windowHeight < windowWidth) {
+    pixelSize = windowHeight / pixelRowAmount;
+    screenOffsetY = 0;
+    screenOffsetX = ..
+  }else{
+    pixelSize = windowWidth / pixelRowAmount;
+  }
+}
+
+function onWindowResize() {
+  resizeCanvas(windowWidth, windowHeight);
+  resizePixels();
 }
